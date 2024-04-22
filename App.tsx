3950f1a5cdcd40ec3d5eb93d77a846bed7b5fae4
Dawid <dawid.jaworski53@proton.me>
2024-04-22 19:10:33 +0200

    Add hot reload

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "@expo/metro-runtime";
 
 export default function App() {
   return (
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
