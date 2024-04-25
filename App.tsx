3ca4f872dd15646a4fbd2854670cc0fa163cc26a
Dawid Jaworski <dawidjaworski53@gmail.com>
2024-04-26 01:23:28 +0200

    assign style marked dates to variable, style fix

--- App.tsx
+++ App.tsx
@@ -8,6 +8,21 @@ export default function App() {
   const dateNow = new Date();
   const currentDate = dateNow.toISOString().split('T')[0];
   const [selected, setSelected] = useState('');
+
+  const customStyle = {
+    container: {
+      borderRadius: 2,
+      borderWidth: 2,
+      borderColor: 'white',
+      backgroundColor: 'transparent',
+      height: '110%',
+      width: '75%',
+    },
+    text: {
+      fontWeight: 'bold',
+    }
+  }
+  
   return (
     <View style={styles.calendarWrapper}>
       <Calendar
@@ -21,9 +36,11 @@ export default function App() {
           calendarBackground: '#141529',
           arrowColor: 'yellow',
           monthTextColor: 'white',
+          dayTextColor: 'white',
         }}
 
         current={currentDate}
+
         onDayPress={day => {
           setSelected(day.dateString);
           console.log('selected day', day)
@@ -31,45 +48,9 @@ export default function App() {
         markingType={'custom'}
         markedDates={{
           [selected]: {selected: true, disableTouchEvent: true, selectedColor: '#06ca88'},
-          '2024-04-01': {selected: true, marked: true, customStyles: {
-            container: {
-              borderRadius: 2,
-              borderWidth: 2,
-              borderColor: 'white',
-              backgroundColor: 'transparent',
-              height: '110%',
-              width: '75%',
-            },
-            text: {
-              fontWeight: 'bold',
-            }
-          }},
-          '2024-04-02': {customStyles: {
-            container: {
-              borderRadius: 2,
-              borderWidth: 2,
-              borderColor: 'white',
-              backgroundColor: 'transparent',
-              height: '110%',
-              width: '75%',
-            },
-            text: {
-              fontWeight: 'bold',
-            }
-          }},
-          '2024-04-03': {selected: true, marked: true, customStyles: {
-            container: {
-              borderRadius: 2,
-              borderWidth: 2,
-              borderColor: 'white',
-              backgroundColor: 'transparent',
-              height: '110%',
-              width: '75%',
-            },
-            text: {
-              fontWeight: 'bold',
-            }
-          }},
+          '2024-04-01': {selected: true, marked: true, customStyles: customStyle},
+          '2024-04-02': {customStyles: customStyle},
+          '2024-04-03': {selected: true, marked: true, customStyles: customStyle},
         }}
       />
     </View>
