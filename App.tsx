d88eb4016ceede5c70f4c808f0a68d6a7c430734
AMJarosz <a.m.jarosz27@gmail.com>
2024-06-03 23:05:58 +0200

    Navigation to AddScreen

--- App.tsx
+++ App.tsx
@@ -1,27 +1,95 @@
 import "@expo/metro-runtime";
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import { Calendar, LocaleConfig } from 'react-native-calendars';
+import { NavigationContainer } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CalendarComp from './components/Calendar'; 
+import AddScreen from './components/AddScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {  
   return (
-    <View style={styles.calendarWrapper}>
-      <CalendarComp/>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({ navigation }) => ({ //tutaj można dodać przycisk zmiany trybu, nawigacja przygotowana, wystarczy wymienić screeny
+            ...headerOptions,
+            headerRight: () => (
+              <TouchableOpacity
+                style={{ marginRight: 10 }}
+                onPress={() => navigation.navigate('name')}>
+                {/* <Text style={{ color: '#fff' }}>Add</Text> */}
+              </TouchableOpacity>
+            ),
+            headerStyle: {
+              backgroundColor: '#302f4b',
+            },
+            headerTintColor: '#fff',
+          })}
+        />
+        <Stack.Screen 
+          name="Back" 
+          component={AddScreen} 
+          options={headerOptions} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.calendarWrapper}>
+        <CalendarComp />
+      </View>
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => navigation.navigate('Back')}>
+        {/* <Text style={{ color: '#fff' }}>Add</Text> */}
+        <Feather name="plus" size={24} color="#fff" />
+      </TouchableOpacity>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
+// Ustawienie koloru headera na każdym ekranie
+const headerOptions = {
+  headerStyle: {
+    backgroundColor: '#302f4b',
+  },
+  headerTintColor: '#fff',
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fffff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   calendarWrapper: {
-    width: '100%',
+    flex: 1,
     backgroundColor: '#302f4b',
+  },
+  addButton: {
+    backgroundColor: '#00FA9A',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 60,
+    height: 60,
+    borderRadius: 30, // połowa wartości width i height dla efektu okrągłości
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    elevation: 3, // efekt cienia na Androidzie
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
   },
 });
---
+++ components/AddScreen.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+
+const AddScreen = () => {
+  return (
+    <View style={styles.background}>
+      <View style={styles.container}>
+        {/* <Text style={styles.text}>Add Screen</Text> */}
+        <TextInput
+          style={styles.input}
+          placeholder="Date"
+          placeholderTextColor="#ccc"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Hour"
+          placeholderTextColor="#ccc"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Task name"
+          placeholderTextColor="#ccc"
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: '#302f4b', // ustaw tło całego ekranu na kolor ciemnoszary
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    width: '80%',
+    height: '60%',
+    backgroundColor: '#141529', // ustaw kolor prostokąta na ciemnoniebieski
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30, // zaokrąglij rogi
+    padding: 20, // dodaj wewnętrzny padding dla odstępów
+  },
+  text: {
+    fontSize: 20,
+    color: '#ffffff', // ustaw biały kolor tekstu
+    marginBottom: 20, // odstęp od góry dla tekstu
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#302f4b',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+});
+
+export default AddScreen;
